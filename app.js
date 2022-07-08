a764e92b850af6ed7ccad144038022e8d174415c
zhuxb <Hello163692163.com>
2022-07-08 22:34:36 +0800

    fix: 获取列表报错

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
+// const session = require("express-session");
+// const RedisStore = require("connect-redis")(session);
 
 const articleRouter = require("./routes/article");
 const userRouter = require("./routes/user");
@@ -15,20 +15,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const redisClient = require("./db/redis");
-const sessionStore = new RedisStore({
-  client: redisClient
-});
+// const redisClient = require("./db/redis");
+// const sessionStore = new RedisStore({
+//   client: redisClient
+// });
 
-app.use(session({
-  secret: "WJiol_1576#",
-  cookie: {
-    path: "/",
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000
-  },
-  store: sessionStore
-}));
+// app.use(session({
+//   secret: "WJiol_1576#",
+//   cookie: {
+//     path: "/",
+//     httpOnly: true,
+//     maxAge: 24 * 60 * 60 * 1000
+//   },
+//   store: sessionStore
+// }));
 
 app.use("/api/article", articleRouter);
 app.use("/api/user", userRouter);
